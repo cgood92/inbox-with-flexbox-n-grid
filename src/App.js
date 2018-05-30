ea1b2c2ac1139c5bc4bbf42c3aab50e968ec84e3
Clint Goodman <clintgoodman@workfront.com>
2018-05-30 08:58:04 -0600

    step1B: add mobile styles

--- src/App.js
+++ src/App.js
@@ -4,20 +4,40 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import LeftSidebar from "./components/LeftSidebar";
 import RightSidebar from "./components/RightSidebar";
+import mq from "./util/mq";
 
 const appStyles = css({
   display: "grid",
-  gridTemplateColumns: "200px 1fr 200px",
+  gridTemplateColumns: "100%",
   gridTemplateRows: "auto 1fr",
   gridTemplateAreas: `
+    "header"
+    "main"
+    "sidebarLeft"
+    "sidebarRight"
+  `,
+  ...mq.medium({
+    gridTemplateColumns: "200px 1fr 200px",
+    gridTemplateRows: "auto 1fr",
+    gridTemplateAreas: `
     "header header header"
     "sidebarLeft main sidebarRight"
   `,
+  }),
   minHeight: "100vh",
   "& > header": {
     gridArea: "header",
     position: "sticky",
     top: 0,
+  },
+  "& > aside:first-of-type": {
+    gridArea: "sidebarLeft",
+  },
+  "& > aside:last-of-type": {
+    gridArea: "sidebarRight",
+  },
+  "& > main": {
+    gridArea: "main",
   },
   "& > *": {
     border: "1px solid gray",
