f8eece9f3d9f9d9872a024752d4120e02a7a0e68
Clint Goodman <clintgoodman@workfront.com>
2018-05-30 09:16:32 -0600

    step2: header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,56 @@
 import React from "react";
+import { css } from "emotion";
+import Box from "../Box";
+import mq from "../../util/mq";
 
-const Header = () => <header>Header</header>;
+const headerStyles = css({
+  display: "flex",
+  alignItems: "center",
+  flexWrap: "wrap",
+  gridArea: "header",
+  zIndex: 100,
+  backgroundColor: "#4285f4",
+  padding: "0 16px 0 8px",
+  color: "white",
+});
+
+const searchContainerStyles = css({
+  flex: "1",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  minWidth: "100%",
+  order: 100,
+  ...mq.medium({
+    minWidth: "auto",
+    order: 0,
+  }),
+});
+
+const searchStyles = css({
+  maxWidth: 800,
+  display: "flex",
+  alignItems: "center",
+});
+
+const Header = () => (
+  <header className={headerStyles}>
+    <Box icon black />
+    <Box icon black>
+      Inbox
+    </Box>
+    <Box height={60} className={searchContainerStyles} noBorder>
+      <Box black width="100%" className={searchStyles} height={50}>
+        <Box icon black className={css({ marginRight: 20 })} />
+        Search
+      </Box>
+      <Box icon black />
+    </Box>
+    <Box icon black className={css({ marginLeft: "auto" })} />
+    <Box icon black />
+    <Box icon black />
+    <Box icon black />
+  </header>
+);
 
 export default Header;
