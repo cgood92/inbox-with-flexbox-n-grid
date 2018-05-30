94986997e06e7f975414d267569f9f8c8432776a
Clint Goodman <clintgoodman@workfront.com>
2018-05-30 08:43:22 -0600

    step1: add css grid

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ import Main from "./components/Main";
 import LeftSidebar from "./components/LeftSidebar";
 import RightSidebar from "./components/RightSidebar";
 
-const appStyles = css({});
+const appStyles = css({
+  display: "grid",
+  gridTemplateColumns: "200px 1fr 200px",
+  gridTemplateRows: "auto 1fr",
+  gridTemplateAreas: `
+    "header header header"
+    "sidebarLeft main sidebarRight"
+  `,
+  minHeight: "100vh",
+  "& > header": {
+    gridArea: "header",
+    position: "sticky",
+    top: 0,
+  },
+  "& > *": {
+    border: "1px solid gray",
+  },
+});
 
 class App extends Component {
   render() {
